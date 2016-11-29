a7ac21129947792c48a3ab5743a66f7bdb3bd267
Guoye Zhang <cc941201@gmail.com>
2016-11-29 02:16:44 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ mvn package &&
 {
   mvn exec:java -Dexec.mainClass="com.oose2016.group4.server.Bootstrap" &
   cd ../SurvivalApp &&
-  xcodebuild test -scheme 'Survival' -destination 'OS=10.1,name=iPhone 7'
+  xcodebuild clean build build-for-testing -scheme 'Survival' -destination 'OS=10.1,name=iPhone 7'
+  xcodebuild test-without-building -scheme 'Survival' -destination 'OS=10.1,name=iPhone 7'
 }
