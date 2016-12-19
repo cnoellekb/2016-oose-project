04ebb7ab25fb7fa0f45fb1521f0e1ff48a84c5fb
Jeana Yee <jeana.yee@gmail.com>
2016-12-18 19:40:16 -0500

    Reverting controller

--- Server/src/main/java/com/oose2016/group4/server/SurvivalController.java
+++ Server/src/main/java/com/oose2016/group4/server/SurvivalController.java
@@ -1,125 +1,85 @@
 package com.oose2016.group4.server;
 
-import java.io.IOException;
-import java.util.List;
+import static spark.Spark.get;
 
-import javax.sql.DataSource;
+import java.util.Collections;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.sql2o.Connection;
-import org.sql2o.Query;
-import org.sql2o.Sql2o;
-import org.sql2o.Sql2oException;
 
 /**
- * Query the database
+ * Setup the web server and handle all requests and responses.
  */
-public class SurvivalService {
-	private Sql2o db;
+public class SurvivalController {
+	private static final String API_CONTEXT = "/v1";
 
+	private SurvivalService survivalService;
 
-	private static Logger logger = LoggerFactory.getLogger(SurvivalService.class);
+	private final Logger logger = LoggerFactory.getLogger(SurvivalController.class);
 
-	public SurvivalService(DataSource dataSource) {
-		db = new Sql2o(dataSource);
-	}
-
-	public Sql2o getDb() {
-		return db;
-	}
-	
-	/**
-	 * Get linkIds to avoid
-	 * @param from top left coordinate
-	 * @param to bottom right coordinate
-	 * @return linkIds
-	 */
-	public AvoidLinkIds getAvoidLinkIds(Coordinate from, Coordinate to) {
-		try (Connection conn = db.open()) {
-			int[] red = fetchLinkIds(conn, from, to, "count > 50");
-			int[] yellow = fetchLinkIds(conn, from, to, "count > 10 AND count <= 50");
-			return new AvoidLinkIds(red, yellow);
-		} catch (Sql2oException e) {
-			logger.error("Failed to fetch linkIds", e);
-			return null;
-		} catch (NullPointerException e) {
-			logger.error("Null pointer, failed to fetch linkIds", e);
-			return null;
-		}
+	public SurvivalController(SurvivalService survivalService) {
+		this.survivalService = survivalService;
+		setupEndpoints();
 	}
 
 	/**
-	 * Create and execute database query
-	 * @param conn database connection
-	 * @param from top left coordinate
-	 * @param to bottom right coordinate
-	 * @param predicate condition
-	 * @return array of linkIds
-	 * @throws Sql2oException when query fails
+	 * Setup handlers for all request paths.
 	 */
-	private int[] fetchLinkIds(Connection conn, Coordinate from, Coordinate to, String predicate)
-			throws Sql2oException, NullPointerException {
-		String sql = "SELECT linkId, COUNT(linkId) AS count FROM crimes WHERE "
-				+ "latitude >= :fromLat AND latitude <= :toLat AND "
-				+ "longitude >= :fromLng AND longitude <= :toLng GROUP BY linkId HAVING " + predicate
-				+ " ORDER BY count DESC LIMIT 20";
-		Query abc = conn.createQuery(sql);
-		abc = abc.addParameter("fromLat", from.getLatitude())
-				.addParameter("toLat", to.getLatitude()).addParameter("fromLng", from.getLongitude())
-				.addParameter("toLng", to.getLongitude());
-		List<AvoidLinkIds.LinkId> results =  abc.executeAndFetch(AvoidLinkIds.LinkId.class);
-		int size = results.size();
-		int[] linkIds = new int[size];
-		for (int i = 0; i < size; i++) {
-			linkIds[i] = results.get(i).getLinkId();
-		}
-		return linkIds;
+	private void setupEndpoints() {
+		/**
+		 * Retrieve linkIDs to Avoid
+		 */
+		get(API_CONTEXT + "/avoidLinkIds", "application/json", (request, response) -> {
+			try {
+				double fromLat = Double.parseDouble(request.queryParams("fromLat"));
+				double fromLng = Double.parseDouble(request.queryParams("fromLng"));
+				double toLat = Double.parseDouble(request.queryParams("toLat"));
+				double toLng = Double.parseDouble(request.queryParams("toLng"));
+				//int timeOfDay = Integer.parseInt(request.queryParams("timeOfDay"));
+				Coordinate from = new Coordinate(fromLat, fromLng);
+				Coordinate to = new Coordinate(toLat, toLng);
+				Coordinate.sortAndExpand(from, to);
+				response.status(200);
+				return survivalService.getAvoidLinkIds(from, to);
+			} catch (Exception e) {
+				logger.info("Invalid request", e);
+				response.status(400);
+				return Collections.EMPTY_MAP;
+			}
+		}, new JsonTransformer());
+		
+		
+		/**
+		 * Get Crime List.
+		 */
+		get(API_CONTEXT + "/crimes", "application/json", (request, response) -> {
+			try {
+				double fromLat = Double.parseDouble(request.queryParams("fromLat"));
+				double fromLng = Double.parseDouble(request.queryParams("fromLng"));
+				double toLat = Double.parseDouble(request.queryParams("toLat"));
+				double toLng = Double.parseDouble(request.queryParams("toLng"));
+				int timeOfDay = Integer.parseInt(request.queryParams("timeOfDay"));
+				int fromDate = Integer.parseInt(request.queryParams("fromDate"));
+				int toDate = Integer.parseInt(request.queryParams("toDate"));
+				//types: <Comma separated Strings>
+				//get array of crimes (int date, String addr, double lat, double lng, String type
+				Crime from = new Crime(fromDate, fromLat, fromLng);
+				Crime to = new Crime(toDate, toLat, toLng);
+				response.status(200);
+				return survivalService.getCrimes(from, to, timeOfDay, "crimes");
+			} catch (Exception e) {
+				logger.info("Invalid request", e);
+				response.status(404); //unsupported location
+				return Collections.EMPTY_MAP;
+			}
+		}, new JsonTransformer());
+		
+		/**
+		 * Do some data preprocessing for database.
+		 */
+		get(API_CONTEXT + "/update/db", "application/json", (request, response) -> {
+			survivalService.updateDB("crimes");
+			return Collections.EMPTY_MAP;
+		}, new JsonTransformer());
 	}
-	
-	/**
-	 * Retrieve all the crimes in the database within a certain time, latitude and longitude 
-	 * range.
-	 * @param from starting crime point
-	 * @param to ending crime point
-	 * @param timeOfDay the time of day
-	 * @return the results of our query to the database
-	 */
-	public List<Crime> getCrimes(Crime from, Crime to, int timeOfDay, String table) {
-		try (Connection conn = db.open()) {
-			String sql = "SELECT date, address, latitude, longitude, type FROM :table WHERE "
-					+ "latitude >= :fromLat AND latitude <= :toLat AND date >= :fromDate AND "
-					+ "longitude >= :fromLng AND longitude <= :toLng AND date <= :toDate;";
-					//+ "time = :timeOfDay"; TODO figure out what to do with this
-			Query query = conn.createQuery(sql);
-			query.addParameter("fromLat", from.getLat()).addParameter("toLat", to.getLat())
-				.addParameter("fromLng", from.getLng()).addParameter("toLng", to.getLng())
-				.addParameter("fromDate", from.getDate()).addParameter("toDate", to.getDate())
-				.addParameter("table", table);
-				//.addParameter("timeOfDay", timeOfDay);
-			List<Crime> results = query.executeAndFetch(Crime.class);
-			return results;
-		} catch (Sql2oException e) {
-			logger.error("Failed to get crimes", e);
-			return null;
-		}	
-	}
-	
-	/**
-	 * Creates or updates the server.db database that holds all of the crime data.
-	 * Only includes data that has all of the fields we need and doesn't have a matching
-	 * compound primary key in the existing data: (date, linkId, type).
-	 */
-	public void updateDB(String table) {
-		try (Connection conn = db.open()){
-			DatabaseUpdater DatabaseUpdater = new DatabaseUpdater(conn);
-			DatabaseUpdater.initialUpdate();
-			DatabaseUpdater.update(table);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (Sql2oException e) {
-			logger.error("Failed to get crimes", e);
-		}
-	}
-
 }
